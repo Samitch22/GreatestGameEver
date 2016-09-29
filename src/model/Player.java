8fb5cf0ca976c5fa56e955f4e9ec09cefe39a816
Mitchell <mitch.jenkins22@gmail.com>
2016-09-29 01:54:35 -0400

    Revert "Update to the View - put in correct folder"

--- src/model/Player.java
+++ src/model/Player.java
@@ -10,7 +10,16 @@ package model;
  */
 public class Player {
     
+    private WordValidator validate;
+    
     public void startGame() {
         // TODO
+    }
+    
+    public void GuessWord(Word guess){
+        Boolean valid = validate.validateWord(guess);
+        if(valid = true) {
+            validate.getNextWord();
+        }
     }
 }
