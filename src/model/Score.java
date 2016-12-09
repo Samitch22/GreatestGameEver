1e1cad5de4dc31311a8238b1781dd54709c70320
Mitchell <mitch.jenkins22@gmail.com>
2016-12-09 11:37:45 -0500

    Added Sams Updates

--- src/model/Score.java
+++ src/model/Score.java
@@ -33,20 +33,7 @@ public class Score implements Serializable {
     private final int        baseMulti = 15;
     private final int        bonus = 5000;
     private       boolean    receivedBonus;
-    
-    
-    // COMMENT OUT defaultScores and scores; ALL OF THE RED NEEDS TO BE MOVED
-    // WE STILL NEED GETTERS AND SETTERS IN THIS SCORE CLASS
-    private final Properties defaultScores;
-    private final Properties scores;
-    private       String     propertyHolder;
-//    private       InputStream in;
-  //  private       OutputStream out;
-    //private final String scoreProperties = "scores.properties";
-    //private final String defaultProperties = "/files/defaultScores.properties";
-    // This might need to be moved too
-    private final String ZERO = "0";
-    
+     
     
     /**
      * Constructs a score holding current score, high score, low score, average 
@@ -57,34 +44,7 @@ public class Score implements Serializable {
         foundWords = new ArrayList<>();
         currentScore = 0;
         receivedBonus = false;
-        // Load default scores
-        this.defaultScores = new Properties();
-        // Default properties stored inside JAR file
-//        this.in = getClass().getResourceAsStream(defaultProperties);
-//        this.defaultScores.load(in);
-//        in.close();
         
-        // Make changes to the stored scores
-        this.scores = new Properties(defaultScores);
-//        try { // Try to read the file
-//            // Modified score properties stored outside JAR file
-////            this.in = new FileInputStream(scoreProperties);
-//        } catch(FileNotFoundException e) { // If file does not exist
-////            out = new FileOutputStream(scoreProperties);
-//            // set defaults from defaultScores
-//            scores.setProperty("highScore", defaultScores.getProperty("highScore"));
-//            scores.setProperty("lowScore", defaultScores.getProperty("lowScore"));
-//            scores.setProperty("averageScore", defaultScores.getProperty("averageScore"));
-//            scores.setProperty("numGames", defaultScores.getProperty("numGames"));
-//  //          scores.store(out, null);
-//  //          out.close();
-//            // Get the file now that it is created
-//  //          this.in = new FileInputStream(scoreProperties);
-//        }
-    //    scores.load(in);
-        
-    //    in.close();
-
     }
     
     /**
@@ -139,13 +99,8 @@ public class Score implements Serializable {
      * @param hScore 
      */
     public void setHighScore(int hScore) {
-        propertyHolder = scores.getProperty("highScore");
-        if ( propertyHolder == null )
-            propertyHolder = this.ZERO;
-        int tempHighScore = Integer.parseInt(propertyHolder);
-        if(currentScore > tempHighScore) {
-            highScore = currentScore;
-            scores.setProperty("highScore", String.valueOf(highScore));
+        if(hScore > highScore) {
+            highScore = hScore;
         }
     }
     
@@ -154,13 +109,8 @@ public class Score implements Serializable {
      * @param lScore 
      */
     public void setLowScore(int lScore) {
-        propertyHolder = scores.getProperty("lowScore");
-        if ( propertyHolder == null )
-            propertyHolder = this.ZERO;
-        int tempLowScore = Integer.parseInt(propertyHolder);
-        if(currentScore < tempLowScore) {
-            lowScore = currentScore;
-            scores.setProperty("lowScore", String.valueOf(lowScore));
+        if(lScore < lowScore) {
+            lowScore = lScore;
         }
     }
     
@@ -169,23 +119,14 @@ public class Score implements Serializable {
      * @param aScore 
      */
     public void setAverageScore(int aScore) {
-        propertyHolder = scores.getProperty("averageScore");
-        if ( propertyHolder == null )
-            propertyHolder = this.ZERO;
-        double tempAverageScore = Double.parseDouble(propertyHolder);
-        propertyHolder = scores.getProperty("numGames", "0");
-        if ( propertyHolder == null )
-            propertyHolder = this.ZERO;
-        int tempNumGames = Integer.parseInt(propertyHolder);
-        numGames = tempNumGames + 1;
+        int tempNumGames = numGames;
+        ++numGames;
         if(tempNumGames == 0) {
             averageScore = currentScore;
         }
         else {
-            averageScore = (tempAverageScore*tempNumGames/numGames) + (currentScore/numGames);
+            averageScore = (averageScore*tempNumGames/numGames) + (aScore/numGames);
         }
-        scores.setProperty("averageScore", String.valueOf(averageScore));
-        scores.setProperty("numGames", String.valueOf(numGames));
     }
     
     /**
@@ -193,8 +134,6 @@ public class Score implements Serializable {
      * @return 
      */
     public int getHighScore() {
-        propertyHolder = scores.getProperty("highScore");
-        highScore = Integer.valueOf(propertyHolder);
         return highScore;
     }
     
@@ -203,8 +142,6 @@ public class Score implements Serializable {
      * @return 
      */
     public int getLowScore() {
-        propertyHolder = scores.getProperty("lowScore");
-        lowScore = Integer.valueOf(propertyHolder);
         return lowScore;
     }
     
@@ -213,8 +150,6 @@ public class Score implements Serializable {
      * @return 
      */
     public int getNumGames() {
-        propertyHolder = scores.getProperty("numGames");
-        numGames = Integer.valueOf(propertyHolder);
         return numGames;
     }
     
@@ -223,23 +158,7 @@ public class Score implements Serializable {
      * @return 
      */
     public double getAverageScore() {
-        propertyHolder = scores.getProperty("averageScore");
-        averageScore = Double.valueOf(propertyHolder);
         return averageScore;
     }
-    
-    /**
-     * Saves the changes made to the Property.
-     */
-    public void save() {
-//        try {
-//            out = new FileOutputStream(scoreProperties);
-//            scores.store(out, "Saved Scores");
-//            out.close();
-//        } catch (FileNotFoundException ex) {
-//            System.out.println("Unexpected Exception: " + ex.getMessage());
-//        } catch (IOException ex) {
-//            System.out.println("Unexpected Exception: " + ex.getMessage());
-//        }
-    }
+
 }
